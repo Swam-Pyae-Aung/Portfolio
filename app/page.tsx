58aacfc921c183676838973cb2cfdb575e357f1c
SwamPyaeAung <168327802+SwamPyaeAung@users.noreply.github.com>
2025-06-19 20:53:27 +0700

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -16,13 +16,13 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        {/* <Hero /> */}
-        {/* <Grid /> */}
-        {/* <RecentProjects /> */}
+        <Hero />
+        <Grid />
+        <RecentProjects />
         {/* <Clients /> */}
-        {/* <Experience /> */}
-        {/* <Approach /> */}
-        {/* <Footer /> */}
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
