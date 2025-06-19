a5aab7af555298489503e8e40cb073e34f91a68b
SwamPyaeAung <168327802+SwamPyaeAung@users.noreply.github.com>
2025-06-19 14:51:45 +0700

    Initial commit

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { projects } from "@/data";
+import { PinContainer } from "./ui/Pin";
+
+const RecentProjects = () => {
+  return (
+    <div className="py-20">
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map((item) => (
+          <div
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            key={item.id}
+          >
+            <PinContainer
+              title="/ui.aceternity.com"
+              href="movie-six-drab.vercel.app"
+            >
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                <div
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  style={{ backgroundColor: "#13162D" }}
+                >
+                  <img src="/bg.png" alt="bgimg" />
+                </div>
+                <img
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute bottom-0"
+                />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {item.title}
+              </h1>
+
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "#BEC1DD",
+                  margin: "1vh 0",
+                }}
+              >
+                {item.des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {item.iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt="icon5" className="p-2" />
+                    </div>
+                  ))}
+                </div>
+                  <a
+  href={item.link}
+  target="_blank"
+  rel="noopener noreferrer"
+  // Add the "group" class to the link itself
+  className="group" 
+>
+  <div className="flex justify-center items-center">
+    <p className="flex lg:text-xl md:text-xs text-sm text-purple group-hover:underline">
+      {/* The "group-hover:underline" will add an underline on hover */}
+      Check Live Site
+    </p>
+    <FaLocationArrow
+      className="ms-3 group-hover:translate-x-2 transition-transform duration-300"
+      //   ↑ Add these classes to the arrow icon
+      color="#CBACF9"
+    />
+  </div>
+</a>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecentProjects;
